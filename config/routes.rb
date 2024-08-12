d33302e0d910a8765aef80af3c81755f0e7216fe
Eduardo Mello <eduardomell06@hotmail.com>
2024-08-11 21:19:12 -0300

    feat:docker compose

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :people
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "people#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -9,3 +9,4 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 end
+
